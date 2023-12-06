2b16311e89c8e739ccc513d6d896ca9528c13d9e
kaveri <patnamkaveri2002@gmail.com>
2023-12-06 20:02:59 +0530

    first commit

---
+++ app1/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from app1.models import Accessrecord, Wedpage,Topic
+
+# Register your models here.
+
+
+admin.site.register(Topic)
+
+admin.site.register(Wedpage)
+
+admin.site.register(Accessrecord)
+
